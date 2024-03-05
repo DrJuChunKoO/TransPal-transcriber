747b180d3137fa81deddfcf4b944a5cefc16d8de
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2024-03-05 13:50:07 +0800

    removed wav2vec align & increase timeout & added timer

--- run_whisperx.py
+++ run_whisperx.py
@@ -1,5 +1,6 @@
 import os
 import modal
+import time
 stub = modal.Stub('transpal-whisperx')
 
 
@@ -12,20 +13,21 @@ stub = modal.Stub('transpal-whisperx')
     .run_commands("pip install git+https://github.com/m-bain/whisperx.git"),
     secrets=[modal.Secret.from_name("my-huggingface-secret")],
     gpu="any",
+    timeout=900
 )
 async def run_whisperx(audio):
     import whisperx
-    import gc
+    start_time = time.time()
 
     device = "cuda"
-    batch_size = 16  # reduce if low on GPU mem
-    # change to "int8" if low on GPU mem (may reduce accuracy)
+    batch_size = 16
     compute_type = "float16"
+
     # 1. Transcribe audio to text
     model = whisperx.load_model(
         "large-v3", device, compute_type=compute_type, asr_options={
             "initial_prompt": "請使用台灣中文",
-            "no_speech_threshold": 0.4,
+            "no_speech_threshold": 0.5,
             "compression_ratio_threshold": 2.2,
         })
 
@@ -33,30 +35,30 @@ async def run_whisperx(audio):
         audio, batch_size=batch_size, language="zh", chunk_size=4)
     print(result["segments"])  # before alignment
 
-    # delete model if low on GPU resources
-    # import gc; gc.collect(); torch.cuda.empty_cache(); del model
+    transcribe_time = time.time() - start_time
 
-    # 2. Align whisper output
-    model_a, metadata = whisperx.load_align_model(
-        language_code=result["language"], device=device, model_name="StevenLimcorn/wav2vec2-xls-r-300m-zh-TW")
-    result = whisperx.align(
-        result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
-
-    print(result["segments"])  # after alignment
-
-    # delete model if low on GPU resources
-    # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
-
-    # 3. Assign speaker labels
+    # 2. Assign speaker labels
     diarize_model = whisperx.DiarizationPipeline(
         use_auth_token=os.environ["HUGGINGFACE_ACCESS_TOKEN"], device=device)
 
-    # add min/max number of speakers if known
     diarize_segments = diarize_model(audio)
-    # diarize_model(audio, min_speakers=min_speakers, max_speakers=max_speakers)
 
     result = whisperx.assign_word_speakers(diarize_segments, result)
     print(diarize_segments)
     print(result["segments"])  # segments are now assigned speaker IDs
 
-    return result["segments"]
+    diarize_time = time.time() - start_time - transcribe_time
+
+    print(f"Transcribe time: {transcribe_time:.2f}s")
+    print(f"Diarize time: {diarize_time:.2f}s")
+    print(f"Total time: {time.time() - start_time:.2f}s")
+
+    result = {
+        "segments": result["segments"],
+        "info": {
+            "transcribe_time": transcribe_time,
+            "diarize_time": diarize_time,
+        }
+    }
+
+    return result
