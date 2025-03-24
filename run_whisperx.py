16bd86f7af8a9c84dc56d7087dc7ca01fae9d645
strawb3rry8ear <191830309+strawb3rry8ear@users.noreply.github.com>
2025-03-24 18:08:03 +0800

    fix: remove file extension check and enhance audio preprocessing in run_whisperx.py

--- run_whisperx.py
+++ run_whisperx.py
@@ -1,6 +1,7 @@
 import os
 import time
 import modal
+import librosa
 
 cuda_version = "12.4.0"  # should be no greater than host CUDA version
 flavor = "devel"  # includes full CUDA toolkit
@@ -21,6 +22,7 @@ image = (
         "git+https://github.com/Hasan-Naseer/whisperX.git@release/latest-faster-whisper-version",
         "ffmpeg-python",
         "ctranslate2==4.4.0",
+        "librosa",
     )
 )
 
@@ -34,7 +36,7 @@ cache_vol = modal.Volume.from_name("whisper-cache", create_if_missing=True)
 @app.function(
     gpu=GPU_CONFIG,
     volumes={CACHE_DIR: cache_vol},
-    allow_concurrent_inputs=1,
+    allow_concurrent_inputs=2,
     scaledown_window=30,
     timeout=60 * 60,
     secrets=[modal.Secret.from_name("my-huggingface-secret")],
@@ -50,11 +52,17 @@ def transcribe(audio_url, bot_token):
         "float16"
     )
 
-    # 1. Transcribe with original whisper (batched)
-    model = whisperx.load_model("large-v3", device, compute_type=compute_type, download_root=CACHE_DIR, asr_options={
-        "no_speech_threshold": 0.5,
-        "compression_ratio_threshold": 2.2,
-    })
+    # Transcribe with original whisper (batched)
+    model = whisperx.load_model(
+        "large-v3", device,
+        compute_type=compute_type,
+        download_root=CACHE_DIR,
+        asr_options={
+            "no_speech_threshold": 0.5,
+            "compression_ratio_threshold": 2.2,
+            "condition_on_previous_text": True,
+        }
+    )
 
     diarize_model = whisperx.DiarizationPipeline(
         use_auth_token=os.environ["HUGGINGFACE_ACCESS_TOKEN"], device=device)
@@ -70,7 +78,16 @@ def transcribe(audio_url, bot_token):
 
     # Transcode
     transcode_start = time.time()
-    audio = whisperx.load_audio("downloaded_audio.wav")
+    audio, sr = librosa.load("downloaded_audio.wav", sr=16000)
+    # 預處理
+    # - 分離人聲和噪音
+    harmonic, percussive = librosa.effects.hpss(audio)
+    audio = harmonic  # 保留諧波部分(人聲)
+    # - 增強高頻
+    audio = librosa.effects.preemphasis(audio)
+    # -  正規化
+    audio = librosa.util.normalize(audio, norm=2)
+
     transcode_time = time.time() - transcode_start
 
     # Transcribe
